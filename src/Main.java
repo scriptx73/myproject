ce7ff51daff68103047eb7aad1d5adbf8bc10b9e
bestzige <bestzige@hotmail.com>
2023-03-27 15:10:02 +0700

    Revert

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Worlds!");
         System.out.println("Hello Worlds!");
+<<<<<<< HEAD
         System.out.println("Hello Worlds!");
         System.out.println("Hello Worlds!");
+=======
+>>>>>>> parent of 7f4bbb3 (Hello Worlds x3)
     }
 
 }
